6d088d5dac94d1f198d6258a1f6f2253473118f0
Emre Altunbilek <emrealtunbilek@gmail.com>
2018-01-02 05:50:36 +0300

    sil ve kopyala butonları aktifleştiirldi

--- app/src/main/java/com/emrealtunbilek/manzararecyclerview/ManzaraAdapter.kt
+++ app/src/main/java/com/emrealtunbilek/manzararecyclerview/ManzaraAdapter.kt
@@ -12,23 +12,20 @@ import kotlinx.android.synthetic.main.tek_satir_manzara.view.*
 /**
  * Created by Emre on 31.12.2017.
  */
-class ManzaraAdapter(tumManzaralar:ArrayList<Manzara>): RecyclerView.Adapter<ManzaraAdapter.ManzaraViewHolder>() {
+class ManzaraAdapter(tumManzaralar: ArrayList<Manzara>) : RecyclerView.Adapter<ManzaraAdapter.ManzaraViewHolder>() {
 
-    var manzaralar=tumManzaralar
+    var manzaralar = tumManzaralar
 
     override fun getItemCount(): Int {
 
-Log.e("RECYCLERVIEW", "GET ITEM COUNT TETIKLENDI")
+      //  Log.e("RECYCLERVIEW", "GET ITEM COUNT TETIKLENDI")
         return manzaralar.size
     }
 
-
-
-
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ManzaraViewHolder? {
 
-        var inflater=LayoutInflater.from(parent?.context)
-        var tekSatirmanzara=inflater.inflate(R.layout.tek_satir_manzara, parent, false)
+        var inflater = LayoutInflater.from(parent?.context)
+        var tekSatirmanzara = inflater.inflate(R.layout.tek_satir_manzara, parent, false)
 
         Log.e("RECYCLERVIEW", "ON CREATE VIEW HOLDER TETIKLENDI")
         return ManzaraViewHolder(tekSatirmanzara)
@@ -37,31 +34,27 @@ Log.e("RECYCLERVIEW", "GET ITEM COUNT TETIKLENDI")
     }
 
 
-
     override fun onBindViewHolder(holder: ManzaraViewHolder?, position: Int) {
 
 
         var oanOlusturulanManzara = manzaralar.get(position)
         holder?.setData(oanOlusturulanManzara, position)
 
-      /*  holder?.manzaraBaslik?.text = manzaralar.get(position).baslik
-        holder?.manzaraAciklama?.text = manzaralar.get(position).aciklama
-        holder?.manzaraResim?.setImageResource(manzaralar.get(position).resim)
-        Log.e("RECYCLERVIEW", "ON BIND VIEW HOLDER TETIKLENDI")*/
+        /*  holder?.manzaraBaslik?.text = manzaralar.get(position).baslik
+          holder?.manzaraAciklama?.text = manzaralar.get(position).aciklama
+          holder?.manzaraResim?.setImageResource(manzaralar.get(position).resim)
+          Log.e("RECYCLERVIEW", "ON BIND VIEW HOLDER TETIKLENDI")*/
 
 
     }
 
 
-
-
-
-    class ManzaraViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+   inner class ManzaraViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
 
         var tekSatirManzara = itemView as CardView
 
-        var manzaraBaslik=tekSatirManzara.tvManzaraBaslik
-        var manzaraAciklama=tekSatirManzara.tvManzaraAciklama
+        var manzaraBaslik = tekSatirManzara.tvManzaraBaslik
+        var manzaraAciklama = tekSatirManzara.tvManzaraAciklama
         var manzaraResim = tekSatirManzara.imgManzara
         var btnKopyala = tekSatirManzara.imgKopyala
         var btnSil = tekSatirManzara.imgSil
@@ -69,25 +62,37 @@ Log.e("RECYCLERVIEW", "GET ITEM COUNT TETIKLENDI")
 
         init {
             Log.e("RECYCLERVIEW", "Manzara view holder TETIKLENDI")
-
-
-
-
         }
 
-        fun setData(oankiManzara:Manzara, position: Int) {
+        fun setData(oankiManzara: Manzara, position: Int) {
+
 
             manzaraBaslik.text = oankiManzara.baslik
             manzaraAciklama.text = oankiManzara.aciklama
             manzaraResim.setImageResource(oankiManzara.resim)
 
             btnKopyala.setOnClickListener {
-                Log.e("EMRE", ""+ position + " kopyalanmak isteniyor")
+
+                manzaralar.add(position, oankiManzara)
+                notifyItemInserted(position)
+                notifyItemRangeChanged(position, manzaralar.size)
+               // notifyDataSetChanged()
+
             }
 
             btnSil.setOnClickListener {
-                Log.e("EMRE", ""+ position + " silinmek isteniyor")
+
+                Log.e("EMRE", "Silinmeden önce item pos : "+position + " Liste size : " + manzaralar.size)
+                manzaralar.removeAt(position)
+                Log.e("EMRE", "Silinmeden sonra item pos : "+position + " Liste size : " + manzaralar.size)
+               notifyItemRemoved(position)
+                notifyItemRangeChanged(position,manzaralar.size)
+               // notifyDataSetChanged()
+
+
             }
+
+
 
         }
 
